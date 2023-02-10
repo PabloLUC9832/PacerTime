cd678636d3a555b901f9d2cab89ab70ca2dd766a
DGAAEA-PROJECT <dgaaeauv.proyecto@gmail.com>
2023-02-10 17:01:42 -0600

    Componente

---
+++ app/View/Components/TimePicker.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class TimePicker extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+
+    public $name;
+    public $message;
+
+    public function __construct($name,$message)
+    {
+        $this->name = $name;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.time-picker');
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'node_modules/flowbite/dist/flowbite.js',
+                'node_modules/flowbite/dist/datepicker.js',
             ],
             refresh: true,
         }),
