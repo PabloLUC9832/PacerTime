0cd4ec4d9de9043368bf8644464859bd55e9da2e
PabloLUC9832 <lucaspablo340@outlook.com>
2023-06-05 23:10:24 -0600

    Boton cancelar

--- resources/views/evento/create.blade.php
+++ resources/views/evento/create.blade.php
@@ -193,8 +193,8 @@
 
                     <div class="grid place-items-center md:flex items-center justify-center">
 
+                        <a href="{{route('eventos.index')}}" type="button" class="focus:outline-none text-white bg-primary-red hover:bg-secondary-red focus:ring-4 focus:ring-red-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Cancelar</a>
 
-                        <button type="button" class="focus:outline-none text-white bg-primary-red hover:bg-secondary-red focus:ring-4 focus:ring-red-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Cancelar</button>
 
                         <button type="submit" class="focus:outline-none text-white bg-primary-yellow hover:bg-secondary-yellow focus:ring-4 focus:ring-yellow-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ">Registrar</button>
 
