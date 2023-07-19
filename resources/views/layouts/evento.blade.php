df39c6e1b4601f09484d36721bf32fa502bee41e
PabloLUC9832 <lucaspablo340@outlook.com>
2023-07-19 14:13:32 -0600

    ajustes

--- resources/views/layouts/evento.blade.php
+++ resources/views/layouts/evento.blade.php
@@ -15,7 +15,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @vite('resources/js/createSubEvento.js')
-    @vite('node_modules/flowbite/dist/flowbite.js')
+    {{--@vite('node_modules/flowbite/dist/flowbite.js')--}}
     @vite('node_modules/flowbite/dist/datepicker.js')
 
 
